b1b3e006e97f5accc1510251b240b06e2a4b9bd4
Alex_Fontaine <alexmcf8@gmail.com>
2025-02-15 15:30:27 -0500

    Created FlashCard class, and Option classes with abstract class.
    options all have the ability to display there message, and calc there point total. Classes C and D can be disabled for T and F questions. All classes contain getters and setters for there respective variables.

---
+++ WinHacks_MainAlex/src/FlashCard/java/dev/alex/OptA.java
@@ -0,0 +1,28 @@
+package dev.alex;
+
+public class OptA extends FlashCard {
+    double time = 0.0;
+
+    public OptA(String A) {
+        super(A);
+        setPoints(5.0);
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    //Displays the text
+    @Override
+    void displayText(){
+        System.out.println("A: " + getText());
+    }
+
+    void CalcPoints(){
+        setPoints(getPoints() - (time*0.2));
+    }
+}
---
+++ WinHacks_MainAlex/src/FlashCard/java/dev/alex/OptB.java
@@ -0,0 +1,28 @@
+package dev.alex;
+
+public class OptB extends FlashCard{
+    public double time = 0.0;
+
+    public OptB(String B) {
+        super(B);
+        setPoints(5.0);
+    }
+
+    public void setTime(double points) {
+        super.setPoints(points);
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    //Displays the text of option
+    @Override
+    void displayText(){
+        System.out.println("B: " + getText());
+    }
+
+    void CalcPoints(){
+        setPoints(getPoints() - (time*0.2));
+    }
+}

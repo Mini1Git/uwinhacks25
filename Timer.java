d15af0fc5fa915da71ec54482a4e8200916211e0
Wilson Tran <wilsontran2017@gmail.com>
2025-02-15 13:28:36 -0500

    made some slight start
    timerStart

--- Timer.java
+++ Timer.java
@@ -1,4 +1,18 @@
+
 public class Timer {
     //make the timer class, that will handle a timer that is ticking down.
+    int seconds;
+  
+    Timer(int s){
+        seconds = s;
+    }
+    public void countDown(){
+        System.out.println(seconds);
+    }
 
+   public static void main(String[] args) {
+       Timer testTimer = new Timer(5);
+       testTimer.countDown();
+       
+   } 
 }

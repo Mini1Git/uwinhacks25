ef5ec0dcb6f0c50cb91aafd2ad29734c24e395c9
Kelcowvine <kelvinjin2005@gmail.com>
2025-02-16 13:43:37 -0500

    fuc ghis
    :middle_finger:

--- FlashCard.java
+++ FlashCard.java
@@ -1,21 +1,23 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 class FlashCard {
-    private String text;
+    private String Question;
     private int nOptions;
     //this boolean is here to check if user answers in the required time. 
     public static boolean answered;
-    private String fileLocation;
     List<String> listofOptions = Arrays.asList("a", "b", "c", "d");
 
+    private HashMap<Integer, ArrayList<String>> answerMap;
+
     
-    public FlashCard(String file){
-        this.fileLocation = file;
+    public FlashCard(){
     }
 
     public FlashCard(String text, int numOptions, String correctAnswer){
-        this.text = text;
+        this.Question = text;
         if (numOptions > 4){
             System.out.println("TOO MANY OPTIONS, PLEASE TRY AGAIN"); //maybe take off this limit if we have time
             System.exit(0);
@@ -28,12 +30,12 @@ class FlashCard {
     //flash card and also states the rules, aka how many options for the one question. // up to 4 options for each question.
     */
     
-    public String getText() {
-        return text;
+    public String getQuestion() {
+        return this.Question;
     }
 
-    public void setText(String text) {
-        this.text = text;
+    public void setQuestion(String text) {
+        this.Question = text;
     }
 
     // now do we want a seperate iption java file, to handle tohte optiomns logic, or do we cuz i was thinking we coauld use this  function to check which is the coererct answer. 
@@ -56,8 +58,9 @@ class FlashCard {
         
     }
 
-    private void fetchSaveInfo(String fileLocations){
-        
+    private void fetchSaveInfo(String fileLocation){
+        SaveFileProcessing sp = new SaveFileProcessing(fileLocation);
+        this.answerMap = sp.returnMap();
     }
 
     public void Play() {
@@ -66,10 +69,14 @@ class FlashCard {
         Thread t1 = new Thread(time); //so basically, 10 sec timer, total points you can get is 10, every 2 seconds that passes it decreases points by 1.
         t1.start();
 
-        
-        //flashcard 
-        FlashCard question = new FlashCard("what color is the wind", 4, "a");
+        //Fetch data on randomized index
+        int randKey = 1;
+        ArrayList<String> fetchList = this.answerMap.get(randKey);
+
+        //flashcard creation
+        FlashCard question = new FlashCard(fetchList.get(0), 4, fetchList.get(2));
         question.displayText();
+        //NOTE: IMPLEMENT WHERE MULTIPLE FLASHCARDS CAN BE USED, WITH MULTIPLE ANSWERS
 
         
 
